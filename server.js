d1c5426b1c3f012799236952f0e70b162abbb62f
ashutosh_k_akhouri <ashutosh.akhouri@gmail.com>
2021-08-30 18:12:08 +0530

    1st

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.static(__dirname+'/dist/newproject'));
+
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname,  '/dist/newproject/index.html');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
